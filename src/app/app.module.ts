089219f9de42bd49fdb17ecf2f9b43f50c74fa41
John Baro <johnbaro@gmail.com>
2018-02-07 18:37:01 +1100

    Final commit - todo: fix tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    RouterModule.forRoot([])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
