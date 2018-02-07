17a82ac2410b249178bd96336d10188968026dbf
John Baro <johnbaro@gmail.com>
2018-02-07 21:18:05 +1100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 @NgModule({
@@ -13,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     MatToolbarModule,
-    RouterModule.forRoot([])
+    AppRoutingModule
 
   ],
   providers: [],
